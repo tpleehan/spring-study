d8cb58309eff5abc254ecf8476dba4d44571fba6
tpleehan <tpleehan@gmail.com>
2021-12-04 00:40:56 +0900

    계정 도메인 commit

---
+++ studylearn/src/test/java/com/studylearn/StudylearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studylearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudylearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
